876be54d966debc58be5c2ce127a2919ed5941bd
POO <sirpepega777@gmail.com>
2023-12-05 09:33:50 -0500

    Ejercicio en clase //Estudiantes

--- src/Juego.java
+++ src/Juego.java
@@ -1,5 +1,6 @@
 public class Juego {
-    public String nombreJuego;
+    private String nombreJuego;
+
     public static void main(String[] args){
         System.out.println("El nombre del juego es, la verdad no me acuerdo");
     }
